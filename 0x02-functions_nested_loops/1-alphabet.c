d52ec04c97eef83cb9121189dc702fe55f8156ce
Nourgoudali <goudalinour2@gmail.com>
2023-06-20 16:57:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i =97 ; i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar (i);
+		_putchar(i);
 	}
 	_putchar('\n');
 }

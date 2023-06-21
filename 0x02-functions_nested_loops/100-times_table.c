b07a6b38ea6d0cbc3c8b91c3dccbc1953be240c0
Nourgoudali <goudalinour2@gmail.com>
2023-06-20 20:07:37 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,40 +6,41 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
+    if (n < 0 || n > 15)
+        return;
 
-	for (int i = 0; i <= n; i++)
-	{
-		for (int j = 0; j <= n; j++)
-		{
-			int result = i * j;
+    int i, j, result;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
 
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
 
-			if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
-			}
-			else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			else
-			{
-				_putchar('0' + result / 100);
-				_putchar('0' + (result / 10) % 10);
-				_putchar('0' + result % 10);
-			}
-		}
-		_putchar('\n');
-	}
+            if (result < 10)
+            {
+                _putchar(' ');
+                _putchar(' ');
+                _putchar('0' + result);
+            }
+            else if (result < 100)
+            {
+                _putchar(' ');
+                _putchar('0' + result / 10);
+                _putchar('0' + result % 10);
+            }
+            else
+            {
+                _putchar('0' + result / 100);
+                _putchar('0' + (result / 10) % 10);
+                _putchar('0' + result % 10);
+            }
+        }
+        _putchar('\n');
+    }
 }

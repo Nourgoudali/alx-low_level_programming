5cecefbcbd5f11e0dfc7c3d70b3970bde9f8324c
Nourgoudali <goudalinour2@gmail.com>
2023-06-21 15:27:34 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,8 +6,9 @@
 int main() {
     int limit = 1024;
     int sum = 0;
+    int i;
 
-    for (int i = 1; i < limit; i++) {
+    for (i = 1; i < limit; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
